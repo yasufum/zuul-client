fb3a3451ac7b590515c42ed27ef701b1723ee11a
Yasufumi Ogawa <yasufum.o@gmail.com>
2022-10-04 01:20:41 +0900

    Support html output
    Signed-off-by: Yasufumi Ogawa <yasufum.o@gmail.com>

--- gerrit_client.py
+++ gerrit_client.py
@@ -10,7 +10,6 @@ BASE_URL = "https://review.opendev.org"
 ZUUL_BASE = "https://zuul.opendev.org"
 
 # Supported output format.
-# TODO: impl output for each formats
 FORMATS = ["json", "html"]
 
 # Status of test result for querying.
@@ -53,6 +52,62 @@ def change_messages(chid):
     return obj
 
 
+def html(json_obj):
+    def make_header(hlist):
+        tmp = ""
+        for h in hlist:
+            tmp = tmp + "<th>" + h + "</th>"
+        return "<thead><tr>{}</tr></thead>".format(tmp)
+
+    hlist = ["No.", "Gerrit URL", "Test Name", "Test Result", 
+            "Start", "End", "Time", "All logs", "Artifacts"]
+
+    html = ""
+
+    js = json_obj
+
+    bs = ""
+    cnt = 1
+    for j in js:
+        jd = j["detail"][0]
+
+        line_html = "<tr>"
+        line_html = line_html + "<td>{num}</td>"
+        line_html = line_html + "<td><a href={g_url}>{g_url}</td>"
+        line_html = line_html + "<td>{name}</td>"
+        line_html = line_html + "<td><a href={result}>result</td>"
+        line_html = line_html + "<td>{t_start}</td>"
+        line_html = line_html + "<td>{t_end}</td>"
+        line_html = line_html + "<td>{time}</td>"
+        line_html = line_html + "<td><a href={logs}>logs</td>"
+        line_html = line_html + "<td><a href={art}>download</td>"
+        line_html = line_html + "</tr>"
+
+        b = line_html.format(
+                num=cnt, g_url=jd["ref_url"], name=j["name"],
+                result=j["url"],
+                t_start=jd["start_time"], t_end=jd["end_time"],
+                time=j["time"],
+                logs=jd["log_url"], art=jd["artifacts"][0]["url"])
+
+        bs = "{}{}".format(bs, b)
+        cnt += 1
+
+
+    thead = make_header(hlist)
+
+    tbody = "<tbody>{}</tbody>".format(bs)
+    html = "<html><table border=1>{}{}</table></html>".format(thead, tbody)
+    print(html)
+
+
+def output(json_obj, format="json"):
+    if format == "html":
+        html(json_obj)
+    else:
+        print(json.dumps(json_obj))
+
+
 def main():
     args = parse_args()
 
@@ -79,7 +134,8 @@ def main():
                 matched = ptn.match(m)
                 if matched is not None:
                     zr = {"name": matched.group(1), "url": matched.group(2),
-                            "time": matched.group(3)}
+                            "time": matched.group(3).replace(" (non-voting)",
+                                "")}
                     zuul_results.append(zr)
 
     for zr in zuul_results:
@@ -90,7 +146,7 @@ def main():
         r = requests.get(req_zuul)
         zr["detail"] = json.loads(r.text)
 
-    print(json.dumps(zuul_results))
+    output(zuul_results, args.format)
 
 if __name__ == '__main__':
     main()
